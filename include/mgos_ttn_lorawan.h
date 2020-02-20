8672b8ecda64180e5da122234b1cee2fe04fe67b
dt0008 <you@example.com>
2020-02-20 15:36:48 +0530

    LORAWAN

---
+++ include/mgos_ttn_lorawan.h
@@ -0,0 +1,19 @@
+#ifndef _MGOS_TTN_LORAWAN_H_
+#define _MGOS_TTN_LORAWAN_H_
+
+#include "arduino_lmic.h"
+#include "../src/hal/hal.h"
+#include <SPI.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif //_MGOS_TTN_LORAWAN_H_
---
+++ src/mgos_ttn_lorawan.c
@@ -0,0 +1,5 @@
+#include <stdbool.h>
+
+bool mgos_ttn_lorawan_init(void) {
+  return true;
+}
